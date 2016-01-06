e7c45498cf5cfee06ee3fcae5f26d6a1f46991bd
John Akers <johnakersemail@gmail.com>
2016-01-06 10:18:36 -0800

    woops... thought i commited

---
+++ lib/algorithm-night.rb
@@ -0,0 +1,7 @@
+require 'algorithm-night/stack'
+require 'algorithm-night/queue'
+require 'algorithm-night/algorithms'
+
+# linked list
+require 'algorithm-night/node'
+require 'algorithm-night/linkedlist'
